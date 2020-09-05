56b612fea0c8e0efeaaa9a31ab54613eeb02fab3
Michael Currin <michael.currin@gmail.com>
2020-09-05 19:43:58 +0200

    feat: Create content

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function renderTemplate(data) {
+    var template = document.getElementById('template').innerHTML;
+    var rendered = Mustache.render(template, data);
+    document.getElementById('target').innerHTML = rendered;
+}
+
+function renderGists(username) {
+    const userGistsUrl = `https://api.github.com/users/${username}/gists`;
+    fetch(userGistsUrl).then(res => res.json()).then(json => renderTemplate({ gists: json }));
+}
