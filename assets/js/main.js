2dddad7969b541dcd521075309c8b79449f81d77
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-06-13 10:13:24 +0200

    refactor: update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,18 @@ function gistsUrl(username, limit = 100) {
   return `https://api.github.com/users/${username}/gists?per_page=${limit}`;
 }
 
-function renderTemplate(data) {
+function renderTemplate(elId, data) {
   const template = document.getElementById('template').innerHTML;
   const rendered = Mustache.render(template, data);
 
-  document.getElementById('target').innerHTML = rendered;
+  document.getElementById(elId).innerHTML = rendered;
 }
 
 async function renderGists(username) {
   const url = gistsUrl(username);
-
   console.debug(url);
 
-  const resp = await fetch(url)
-  const json = await resp.json()
-  renderTemplate({ gists: json })
+  const resp = await fetch(url);
+  const json = await resp.json();
+  renderTemplate('target', { gists: json });
 }
