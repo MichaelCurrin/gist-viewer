8c0ce85e87a590fcb374265a9d1b3ea6022ec843
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-06-14 10:27:02 +0200

    style: update Gists.js

--- assets/js/Gists.js
+++ assets/js/Gists.js
@@ -21,7 +21,7 @@ const Gists = {
       gists: null,
       loading: true,
       errored: false,
-      errorMsg: '',
+      errorMsg: "",
     };
   },
   methods: {
@@ -29,38 +29,38 @@ const Gists = {
       try {
         const resp = await fetch(url);
         if (!resp.ok) {
-          throw new Error(`${resp.status} ${resp.statusText}`)
+          throw new Error(`${resp.status} ${resp.statusText}`);
         }
         const gists = await resp.json();
         this.gists = gists;
       } catch (err) {
-        const msg = `Unable to fetch Gists API data. Error: ${err}`
+        const msg = `Unable to fetch Gists API data. Error: ${err}`;
         console.error(msg);
         this.errored = true;
-        this.errorMsg = msg
+        this.errorMsg = msg;
       } finally {
         this.loading = false;
       }
     },
     sortBy(field) {
       if (!this.gists) {
-        console.error('Nothing to sort')
-        return
+        console.error("Nothing to sort");
+        return;
       }
       this.gists.sort((a, b) => (a[field] > b[field] ? 1 : -1));
     },
     async render() {
       const url = gistsUrl(this.username);
 
-      console.debug(`Fetching gists: ${url}`)
+      console.debug(`Fetching gists: ${url}`);
       await this.fetchGists(url);
 
-      console.debug('Sorting')
+      console.debug("Sorting");
       this.sortBy("description");
-    }
+    },
   },
   async mounted() {
-    this.render()
+    this.render();
   },
   template: `
     <section>
